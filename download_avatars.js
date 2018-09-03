203de2668994013f656487c61a77ba34ed4cc5bf
Yang Yu <nicholas.yu24@gmail.com>
2018-09-03 23:27:15 +0000

    add new function and token file

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,19 @@
+var request = require('request');
+
+getRepoContributors("nicholasyyu", "GitHub-Avatar-Downloader", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
+
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': ''
+    }
+  }
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
 }
