1a3572dfc90a6e214fb95cf727312591bc65611b
Yang Yu <nicholas.yu24@gmail.com>
2018-09-04 00:27:36 +0000

    add avatar download

--- download_avatars.js
+++ download_avatars.js
@@ -1,8 +1,15 @@
 var request = require('request');
+var fs = require('fs');
+var getToken = require('./secrets');
 
-getRepoContributors("nicholasyyu", "GitHub-Avatar-Downloader", function(err, result) {
+var object = {};
+var filePath = "avatars/";
+
+getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
-  console.log("Result:", result);
+  object = JSON.parse(result);
+  console.log("Errors:", object);
+  getAvatarURL(object);
 });
 
 function getRepoContributors(repoOwner, repoName, cb) {
@@ -10,10 +17,23 @@ function getRepoContributors(repoOwner, repoName, cb) {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request',
-      'Authorization': ''
+      'Authorization': getToken.GITHUB_TOKEN
     }
   }
   request(options, function(err, res, body) {
     cb(err, body);
   });
+}
+
+function getAvatarURL(object){
+  for(var i = 0; i < object.length; i++){
+    //console.log('avatar_url: ' + object[i].avatar_url);
+    downloadImageByURL(object[i].avatar_url, filePath + object[i].login);
+  }
+}
+
+function downloadImageByURL(url, filePath) {
+  // ...
+  request(url).pipe(fs.createWriteStream(filePath));
+
 }
